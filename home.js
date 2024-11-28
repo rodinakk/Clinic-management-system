429349b8c8a73a9fccc9cba2dbff20b6407330f4
rodinakk <160980034+rodinakk@users.noreply.github.com>
2024-11-28 20:07:24 +0200

    css

---
+++ home.js
@@ -0,0 +1,22 @@
+function home() {
+    
+  window.location.href = "clinichome.html"; 
+}
+
+function book() {
+    
+    window.location.href = "booknow.html"; 
+  }
+  
+  function view1() {
+    
+    window.location.href = "https://www.w3schools.com/js/js_window_location.asp"; 
+  }
+  function view2() {
+    
+    window.location.href = "https://www.w3schools.com/js/js_window_location.asp"; 
+  }
+  function view3() {
+    
+    window.location.href = "https://www.w3schools.com/js/js_window_location.asp"; 
+  }
